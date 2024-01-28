c0cf255c60c34e270314565461770e22712aa478
tanishq <tppokhari@gmail.com>
2024-01-28 18:03:11 +0530

    mediaquery and splash screen added

--- lib/mediaquery/media_query.dart
+++ lib/mediaquery/media_query.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class MQ {
   static double adaptiveSize(BuildContext context, double size) {
-    return MediaQuery.of(context).size.height * size;
+    return MediaQuery.of(context).size.height * (size / 1000);
   }
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -14,34 +14,35 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-              padding: EdgeInsets.all(MQ.adaptiveSize(context, 10)),
-              margin: EdgeInsets.all(MQ.adaptiveSize(context, 10)),
-              height: MQ.adaptiveSize(context, 150),
-              width: MQ.adaptiveSize(context, 150),
-              child: Image.asset("assets/nex.png")),
-          Shimmer(
-            // period: Duration(milliseconds: 100),
-            direction: ShimmerDirection.ltr,
-            gradient: const LinearGradient(colors: [
-              Colors.white,
-              Colors.yellow,
-              Colors.pink,
-              Colors.blue,
-              Colors.cyan
-            ]),
-            child: Text(
-              "NexSync",
-              style: TextStyle(fontSize: MQ.adaptiveSize(context, 40)),
-            ),
-          )
-        ],
-      )),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                padding: EdgeInsets.all(MQ.adaptiveSize(context, 10)),
+                margin: EdgeInsets.all(MQ.adaptiveSize(context, 10)),
+                height: MQ.adaptiveSize(context, 150),
+                width: MQ.adaptiveSize(context, 150),
+                child: Image.asset("assets/nex.png")),
+            Shimmer(
+              // period: Duration(milliseconds: 100),
+              direction: ShimmerDirection.ltr,
+              gradient: const LinearGradient(colors: [
+                Colors.white,
+                Colors.yellow,
+                Colors.pink,
+                Colors.blue,
+                Colors.cyan
+              ]),
+              child: Text(
+                "NexSync",
+                style: TextStyle(fontSize: MQ.adaptiveSize(context, 40)),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
