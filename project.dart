16594be54d3fc79cb99534357b077daafb426d3e
Ritinikhil <124866156+Ritinikhil@users.noreply.github.com>
2024-01-31 11:28:38 +0530

    Project Screen

---
+++ project.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class project extends StatefulWidget {
+  const project({Key? key}) : super(key: key);
+
+  @override
+  State<project> createState() => _projectState();
+}
+
+class _projectState extends State<project> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
